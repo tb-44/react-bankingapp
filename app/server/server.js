1f2658552bd5ae476426b9d7e5cec0b178c2c4ad
Trent Bennett <rehab.rtb@gmail.com>
2017-09-07 13:26:47 -0600

    new changes

--- app/server/server.js
+++ app/server/server.js
@@ -25,6 +25,51 @@ massive(process.env.CONNECTIONSTRING).then( db => {
 });
 
 //AUTHENTICATION
+passport.use(new Auth0Strategy({
+  domain: process.env.AUTH_DOMAIN,
+  clientID: process.env.AUTH_CLIENT_ID,
+  clientSecret: process.env.AUTH_CLIENT_SECRET,
+  callbackURL: process.env.AUTH_CALLBACK
+}, function(accessToken, refreshToken, extraParams, profile, done){
+     console.log(profile);
+
+  const db = app.get('db');
+  db.find_user(profile.id).then( user => {
+    console.log(user);
+    if(user[0]) {
+      return done(null, user);
+    } else {
+      db.create_user([profile.displayName, profile.emails[0].value,
+      profile.picture, profile.id]).then(user => {
+        return done(null, user[0]);
+      })
+    }
+  })
+}));
+
+//THIS IS INVOKED ONE TIME TO SET THINGS UP
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+//USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
+passport.deserializeUser(function(user, done){
+  app.get('db').find_session_user(user[0].id).then(user => {
+    return done(null, user[0]);
+  })
+});
+
+//SET UP OUR ENDPOINTS (NEED 4 ENDPOINT PER OUR DESIGN)
+//ENDPOINT #1
+app.get('/auth', passport.authenticate('auth0'));
+//ENDPOINT #2
+app.get('/auth/callback', passport.authenticate('auth0', {
+  successRedirect: 'http://localhost:3000/#/private',
+  failureRedirect: 'http://localhost:3000/#/'
+}))
+//ENDPOINT #3
+
+
 
 let PORT = 3005;
 app.listen(PORT, () => {
