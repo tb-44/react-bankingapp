a3744cdd50ee6f74345104628dad691a515acb0c
Trent Bennett <rehab.rtb@gmail.com>
2017-09-07 16:18:03 -0600

    Private changes

--- app/src/components/Private/Private.js
+++ app/src/components/Private/Private.js
@@ -13,8 +13,19 @@ class Private extends Component {
     console.log(this.props.user);
     return (
       <div>
-        <h1>Private</h1>
-      </div>
+             <h1>Community Bank</h1>
+             <div className='accountInfoContainer'>
+                 <h4>Account Information:</h4>
+                     {this.props.user ? <img className='avatar' src={this.props.user.img} alt='' /> : null }
+                 <div>
+                     <p>Username: { this.props.user ? this.props.user.user_name : null }</p>
+                     <p>Email: { this.props.user ? this.props.user.email : null }</p>
+                     <p>ID: { this.props.user ? this.props.user.id : null }</p>
+                     <h4>Available Balance: { this.props.user ? '$' + Math.floor((Math.random() + 1 ) * 100) + '.00' : null }</h4>
+                 </div>
+                     <a href={process.env.REACT_APP_LOGOUT}><button>Logout</button></a>
+             </div>
+         </div>
     )
   }
 }
